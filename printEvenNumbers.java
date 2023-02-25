acaf049d16e89ddc2202c1905b4118f367bfbd42
DecimoDevs <marlonadiguemartin548@gmail.com>
2023-02-25 14:51:20 +0800

    p7

---
+++ printEvenNumbers.java
@@ -0,0 +1,13 @@
+public class printEvenNumbers {
+    public static void main(String[] args) {
+        
+        int n = 50;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0 ){
+                System.out.println("Even Number : " + i);
+            }
+        } 
+
+    }
+}
