21fe868dd8b6358b4c7bc320e2c5af17014846b3
DecimoDevs <marlonadiguemartin548@gmail.com>
2023-02-25 14:53:32 +0800

    p8

---
+++ checkIfItsEvenOrOdd.java
@@ -0,0 +1,18 @@
+public class checkIfItsEvenOrOdd {
+    
+    public static void main(String[] args) {
+    
+        int n = 50;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 != 0) {
+                System.out.println("Odd Number: " + i);
+            } else {
+                System.out.println("Even Number: " + i);
+            }
+
+        }
+
+    }
+
+}
